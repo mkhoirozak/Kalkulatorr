3745e6ae7a4a8e59d44c3bcd841cefbb5ac14de3
mkhoirozak <95408152+mkhoirozak@users.noreply.github.com>
2021-12-02 18:03:47 +0700

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Kalkulator
+    {
+        public string Hasilnya { get; set; }
+    }
+}
+namespace Calculator
+{
+    public partial class HasilHitung : Form
+    public HasilHitung()
+    {
+        InitializeComponent();
+        initialisasilv();
+    }
+
+    private void initialisasilv()
+    {
+        lvHasil.View = view.Details;
+
+        lvHasil.Columns.Add(**, 10, HorizontalAlignment.Left);
+        lvHasil.Columns.Add(**, 200, HorizontalAligment.Left);
+
+    }
+
+    private void Frmcalc_OnCalculated(Kalkulator calc)
+    {
+        listDariHasil.Add(calc);
+
+        ListViewItem item = new ListViewItem();
+        item.Subitems.Add(calc.Hasilnya);
+
+        lvHasil.Item.Add(item);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+
+    }
+}   
